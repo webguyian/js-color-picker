5be51bca091566bf28a2dc25ee96cd3242acfe53
Ian Mac <webguyian@gmail.com>
2014-01-19 10:55:28 -0500

    Add input sliders

--- script.js
+++ script.js
@@ -1,118 +1,79 @@
 var doc = document,
-		canvas = doc.getElementById('colorpicker'),
-		ctx = canvas.getContext('2d'),
-		hexOutput = doc.querySelector('.output-hex'),
-		rgbOutput = doc.querySelector('.output-rgb');
+canvas = doc.getElementById('colorpicker'),
+ctx = canvas.getContext('2d'),
+hexOutput = doc.getElementsByName('output-hex')[0],
+rgbOutput = doc.getElementsByName('output-rgb')[0],
+redOutput = doc.querySelector('.output-red'),
+greenOutput = doc.querySelector('.output-green'),
+blueOutput = doc.querySelector('.output-blue'),
+colorInputs = doc.querySelectorAll('.input'),
+redInput = doc.getElementsByName('input-red')[0],
+greenInput = doc.getElementsByName('input-green')[0],
+blueInput = doc.getElementsByName('input-blue')[0];
+
+var colorpicker = {
+
+	init: function() {
+		for (var i = 0; i < colorInputs.length; i++) {
+			colorInputs[i].addEventListener('change', this.updateValues, false);
+		}
+		canvas.addEventListener('mousemove', this.getPosition, false);
+		makeGradient();
+	},
+
+	updateValues: function() {
+		var target = this.dataset.target,
+		output = doc.querySelector('.output-' + target);
+		output.innerHTML = this.value;
+		makeGradient();
+		return this.value;
+	},
+
+	getPosition: function(evt) {
+		var x = evt.pageX - this.offsetLeft,
+		y = evt.pageY - this.offsetTop,
+		coordinates = [];
+		coordinates.push(x);
+		coordinates.push(y);
+		cp.returnImageData(coordinates);
+		return coordinates;
+	}
+};
 
 function rgbToHex(R,G,B) {
-	return toHex(R)+toHex(G)+toHex(B);
+	return '#' + toHex(R)+toHex(G)+toHex(B);
 }
 
 function toHex(n) {
-	var n = parseInt(n,10),
-			hexNum = "0123456789ABCDEF".charAt((n-n%16)/16);
-	if (isNaN(n)) {
-		return "00";
+	var hex = n.toString(16);
+	return hex.length == 1 ? "0" + hex : hex;
+}
+
+function makeGradient() {
+	var r = redInput.value,
+			g = greenInput.value,
+			b = blueInput.value,
+			rgb = 'rgb(' + r + ', ' + g + ', ' + b + ')',
+			gradient = ctx.createLinearGradient(0, 0, 700, 256);
+	gradient.addColorStop(0, rgb);
+	gradient.addColorStop(1, 'white');
+	ctx.fillStyle = gradient;
+	ctx.fillRect(0, 0, 700, 256);
+}
+
+function returnImageData(mousePosition) {
+		var coordinates = mousePosition,
+				x = coordinates[0],
+				y = coordinates[1],
+				imgData = ctx.getImageData(0, 0, canvas.width, canvas.height).data,
+				red = imgData[0],
+				green = imgData[1],
+				blue = imgData[2],
+				rgb = red + ', ' + green + ', ' + blue;
+		rgbOutput.value = rgb;
+		hexOutput.value = rgbToHex(red, green, blue);
+		return rgb;
 	}
-	n = Math.max(0,Math.min(n,255));
-	var hexcode = hexNum + hexNum + hexNum  + hexNum + hexNum + hexNum;
-	return "#" + hexcode;
-}
 
-function createSpan(colors) {
-	var span = doc.createElement('span'),
-	colorVal = colors.join(),
-	bgColor = "rgb(" + colorVal + ")";
-	console.log(bgColor);
-	span.className = 'block';
-	span.style.backgroundColor = bgColor;
-	span.setAttribute('data-color', '#' + rgbToHex(colors[0], colors[1], colors[2]));
-	canvas.appendChild(span);
-}
+colorpicker.init();
 
-function makeRectangles(colors) {
-	var colorVal = colors.join(),
-			bgColor = "rgb(" + colorVal + ")";
-	for (var i = 0; i < 50; i++) {
-		var x = 0,
-				y = 0;
-		ctx.fillStyle = bgColor;
-		ctx.fillRect(x, y, 50, 50);
-		x += 10;
-		y += 10;
-	}
-}
-
-function makeGrays() {
-	var R = 0,
-			G = 0,
-			B = 0,
-			x = 0,
-			y = 0;
-
-	for (var i = 0; i < 256; i++) {
-		var rgb = R + ', ' + G + ', ' + B,
-				bgColor = "rgb(" + rgb + ")"; 
-		ctx.fillStyle = bgColor;
-		ctx.fillRect(x, y, 700, 1);
-		R++;
-		G++;
-		B++;
-		y++;
-	}
-}
-
-function makeReds() {
-	var r = 0,
-	g = 0,
-	b = 0;
-
-	for (var i = 0; i < 256; i++) {
-		var colorArray = [r, g, b];
-		g++;
-		b++;
-		createSpan(colorArray);
-	}
-}
-
-function makeGreens() {
-	var r = 0,
-	g = 0,
-	b = 0;
-
-	for (var i = 0; i < 256; i++) {
-		var colorArray = [r, g, b];
-		r++;
-		b++;
-		createSpan(colorArray);
-	}
-}
-
-function makeBlues() {
-	var r = 0,
-	g = 0,
-	b = 0;
-
-	for (var i = 0; i < 256; i++) {
-		var colorArray = [r, g, b];
-		r++;
-		g++;
-		createSpan(colorArray);
-	}
-}
-
-function getPosition(evt) {
-	var x = evt.pageX - this.offsetLeft,
-			y = evt.pageY - this.offsetTop,
-			imgData = ctx.getImageData(x, y, 1, 1).data,
-			R = imgData[0],
-			G = imgData[1],
-			B = imgData[2],
-			rgb = R + ', ' + G + ', ' + B;
-	rgbOutput.value = rgb;
-	hexOutput.value = toHex(rgb);
-	// console.log(x, y);
-}
-
-canvas.addEventListener('click', getPosition, false);
-makeGrays();
